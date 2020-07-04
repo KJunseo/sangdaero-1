0c252e20eee8da36118ea3457721b9880de4175b
KJunseo <max9106@naver.com>
2020-07-05 01:35:03 +0900

    공지사항 css

--- src/main/java/com/sangdaero/walab/notice/service/NoticeService.java
+++ src/main/java/com/sangdaero/walab/notice/service/NoticeService.java
@@ -267,4 +267,8 @@ public class NoticeService extends CategoryService {
 				.memo(category.getMemo())
 				.build();
 	}
+
+//    public Long getAllNoticeNum(Long category) {
+//    	return mNoticeRepository.countByTopCategory(category);
+//    }
 }
